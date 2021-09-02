7afd9e08b4ca80507b9c3e8e8b3306fd25c07f9a
robertocahuana04 <robertocahuana04@hotmail.com>
2021-09-02 13:38:06 -0500

    mismo

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_09_02_025425) do
+
+  create_table "productos", force: :cascade do |t|
+    t.string "codigo"
+    t.string "nombre"
+    t.string "referencia"
+    t.integer "stock"
+    t.string "remitente"
+    t.integer "tipo_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tipos", force: :cascade do |t|
+    t.string "tipo"
+    t.boolean "estado"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
