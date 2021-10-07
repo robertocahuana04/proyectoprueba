cd1f05a9699f1db90b8271915a19d42e40721043
robertocahuana04 <robertocahuana04@hotmail.com>
2021-10-06 19:04:54 -0500

    4 mas

---
+++ app/controllers/usuarios_controller.rb
@@ -0,0 +1,75 @@
+class UsuariosController < ApplicationController
+
+    def index
+        @usuario = Usuario.all
+    end
+
+    def new
+        @usuario = Usuario.new   
+    end
+
+    def show
+        @usuario = Usuario.find(params[:id])   
+    end
+
+    def edit
+      @usuario = Usuario.find(params[:id])
+    end
+
+    def create   
+        @usuario = Usuario.new(usuario_params)   
+        if @usuario.save   
+          flash[:notice] = 'usuario agregado!'   
+          redirect_to (@usuario)   
+        else   
+          flash[:error] = 'No se pudo editar el usuario!'   
+          render :new   
+        end   
+    end   
+     
+    private
+
+    def usuario_params
+        params.require(:usuario).permit(
+          :identificacion,
+          :nombre,
+          :telefono,
+          :direccion,
+        )
+    end
+
+    def update
+      @usuario = Usuario.find(params[:id])
+     
+      if @usuario.update(usuario_params)
+        redirect_to @usuario
+      else
+        render 'edit'
+      end
+  end
+
+  private
+
+  def usuario_params
+      params.require(:usuario).permit(
+        :identificacion,
+          :nombre,
+          :telefono,
+          :direccion,
+      )
+  end
+
+  def destroy
+    @usuario = Usuario.find(params[:id])
+    if @usuario.destroy
+        flash[:success] = 'usuario was successfully deleted.'
+        redirect_to @usuario
+    else
+        flash[:error] = 'Something went wrong'
+        redirect_to usuario
+    end
+  end
+
+    
+
+end    
