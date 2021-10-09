cf38cba5ced4f0d041a939479e231f7e870a3b62
robertocahuana04 <robertocahuana04@hotmail.com>
2021-10-08 21:54:18 -0500

    1

--- app/controllers/productos_controller.rb
+++ app/controllers/productos_controller.rb
@@ -1,40 +1,96 @@
 class ProductosController < ApplicationController
 
+
   before_action :authenticate_user!
 
   # before_action :authenticate_user!, only: [:index]
 
+  def new
+    #get /producto/new
+      @producto = Producto.new   
+  end
+
   def index
+    #todo los registro SELET FRON productos
     @producto = Producto.all
   end
 
   def show
+    #encontrar un registro por id
+      @producto = Producto.find(params[:id])           
+  end
+
+  def edit
     @producto = Producto.find(params[:id])
   end
 
-  def new
-    @producto = Producto.new
+  def create   
+      @producto = Producto.new(producto_params)   
+      if @producto.save   
+        flash[:notice] = 'Producto agregado!'   
+        redirect_to (@producto)   
+      else   
+        flash[:error] = 'No se pudo editar el productos!'   
+        render "new"   
+      end   
+  end   
+   
+  private
+
+  def producto_params
+      params.require(:producto).permit(
+        :codigo,
+        :nombre,
+        :referencia,
+        :stock,
+        :remitente,
+      )
   end
 
-  def create
-    @producto = Producto.new( producto_params[:id])
-    if @producto.save 
-        redirect_to producto_path(@producto)
-    else
-        render :new
-    end
-end
- 
-private
+  
+  def update
+      @producto = Producto.find(params[:id])
+      if @producto.update(producto_params)
+        redirect_to @producto
+      else
+        @producto = Producto.all
+        render "edit", status: :unprocessable_entity
+      end
+  end
 
-def producto_params
-    params.require(:producto).permit(
-      :codigo,
-      :nombre,
-      :referencia,
-      :stock,
-      :remitente,
-      :tipo,
-    )
-end
+  private
+
+  def producto_params
+      params.require(:producto).permit(
+        :codigo,
+        :nombre,
+        :referencia,
+        :stock,
+        :remitente
+      )
+  end
+   
+  def destroy 
+      #DELETE /producto/id  
+      @producto = Producto.find(params[:id])   
+      if @producto.destroy   #Destroy eliminar el objeto de la BD
+        flash[:notice] = '¡Producto eliminado!'  
+        redirect_to producto_path 
+      else   
+        flash[:error] = '¡Error al eliminar este Producto!'   
+        render "destroy"   
+      end   
+  end
+
+  private
+
+  def producto_params
+      params.require(:producto).permit(
+        :codigo,
+        :nombre,
+        :referencia,
+        :stock,
+        :remitente,
+      )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
+  
+  resources :productos
   get 'productos/index'
   get 'productos/show'
   #devise_for :users
